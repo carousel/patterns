a979219d9dfbd058a94d2f6aca4748b6febcdeb6
Miroslav Trninic <miroslav.trninic@gmail.com>
2017-02-09 17:31:47 +0100

    cloning

--- patterns/index.php
+++ patterns/index.php
@@ -1,2 +1,18 @@
 <?php
+    class Cell {}
+    
+        class CellFactory {
+            
+            public function makeCell()
+            {
+                $cell = clone new Cell;
+                return $cell;
+            }
+        }
+        $factory1 = new CellFactory;
+        $factory2 = new CellFactory;
+        $cell1 = $factory1->makeCell();
+        $cell2 = $factory2->makeCell();
+        echo $cell1 == $cell2;
+    
 
